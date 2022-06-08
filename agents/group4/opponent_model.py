c4ded009103c65765093ab0b9d2c0e2ddbf9ac27
Arash <arash.mehrabi@ozu.edu.tr>
2022-06-08 14:06:54 +0300

    Add frequency opponent modeling to Group4 agent

--- agents/group4/opponent_model.py
+++ agents/group4/opponent_model.py
@@ -22,6 +22,18 @@ class OpponentModel:
         self.offers = []
 
         self.issues = {issue: Issue(values) for issue, values in domain.getIssuesValues().items()}
+        init_weight = 1 / len(self.issues)
+        for issue_name, issue_obj in self.issues.items():
+            issue_obj.set_weight(init_weight)
+
+        self.n = 0.1
+
+    def normalize_issue_weights(self, total_weight):
+        if total_weight - 1 > 0.001:
+            difference_per_issue = (total_weight - 1) / len(self.issues)
+            for issue_name, issue_obj in self.issues.items():
+                issue_obj.set_weight(issue_obj.get_weight() - difference_per_issue)
+
 
     def update(self, bid: Bid, **kwargs):
         """
@@ -34,11 +46,30 @@ class OpponentModel:
         if bid is None:
             return
 
+        if len(self.offers) > 0:
+            last_offer = self.offers[-1]
+
+            total_weight = 0.0
+            for issue_name, issue_obj in self.issues.items():
+                weight = issue_obj.get_weight()
+                if bid.getValue(issue_name) == last_offer.getValue(issue_name):
+                    weight = issue_obj.get_weight() + self.n
+                    issue_obj.set_weight(weight)
+                total_weight += weight
+
+            self.normalize_issue_weights(total_weight)
+
         self.offers.append(bid)
 
         # Call each issue object with corresponding received value.
         for issue_name, issue_obj in self.issues.items():
             issue_obj.update(bid.getValue(issue_name), **kwargs)
+
+        for issue_name, issue_obj in self.issues.items():
+            issue_obj.update_value_weights(**kwargs)
+
+        # for issue_name, issue_obj in self.issues.items():
+        #     print(issue_name, issue_obj.weight)
 
     def get_utility(self, bid: Bid) -> float:
         """
@@ -56,17 +87,34 @@ class OpponentModel:
 
         return total
 
-
 class Issue:
     """
         This class can be used to estimate issue weight and value weights.
     """
     weight: float = 0.0    # Issue Weight
     value_weights: dict    # Value Weights
+    num_occurences: dict
 
     def __init__(self, values: DiscreteValueSet, **kwargs):
         # Initial value weights are zero
         self.value_weights = {value: 0.0 for value in values}
+        self.num_occurences = dict()
+
+    def get_weight(self):
+        return self.weight
+
+    def set_weight(self, weight):
+        self.weight = weight
+
+    def update_value_weights(self, **kwargs):
+        max_occurences = -1
+
+        for value, occurences in self.num_occurences.items():
+            if occurences > max_occurences:
+                max_occurences = occurences
+
+        for value, occurences in self.num_occurences.items():
+            self.value_weights[value] = occurences / max_occurences
 
     def update(self, value: Value, **kwargs):
         """
@@ -77,7 +125,10 @@ class Issue:
         if value is None:
             return
 
-        pass
+        if not value in self.num_occurences:
+            self.num_occurences[value] = 1
+        else:
+            self.num_occurences[value] += 1
 
     def get_utility(self, value: Value) -> float:
         """
